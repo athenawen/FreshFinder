ecf511174aa3915e7241435efb901111f0ab9a5c
athenawen <athena.wen21@gmail.com>
2024-09-04 10:08:47 -0500

    Initial Commit

---
+++ Cookcademy/ViewModels/RecipeData.swift
@@ -0,0 +1,41 @@
+//
+//  RecipeData.swift
+//  Cookcademy
+//
+//  Created by Ben Stone on 4/19/21.
+//
+
+import Foundation
+
+class RecipeData: ObservableObject {
+    @Published var recipes = Recipe.testRecipes
+    
+    func recipes(for category: MainInformation.Category) -> [Recipe] {
+        var filteredRecipes = [Recipe]()
+        for recipe in recipes {
+            if recipe.mainInformation.category == category {
+                filteredRecipes.append(recipe)
+            }
+        }
+        return filteredRecipes
+    }
+    
+    var favoriteRecipes: [Recipe] {
+        recipes.filter { $0.isFavorite }
+    }
+    
+    func add(recipe: Recipe) {
+        if recipe.isValid {
+            recipes.append(recipe)
+        }
+    }
+    
+    func index(of recipe: Recipe) -> Int? {
+        for i in recipes.indices {
+            if recipes[i].id == recipe.id {
+                return i
+            }
+        }
+        return nil
+    }
+}
